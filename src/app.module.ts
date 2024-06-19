93119d384b4f80727b9d678893115cec3c666109
Guilherme Ferreira de Souza <guilherme.ferreira@venturus.org.br>
2024-06-18 21:29:25 -0300

    Env vars load

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
-import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from './config/config.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
