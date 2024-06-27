1948e151d0b891e9ac5e6db056b2a485d841589e
Guilherme Ferreira de Souza <guilherme.ferreira@venturus.org.br>
2024-06-27 01:09:35 +0000

    End to end tests

--- src/nest-modules/shared-modules/testing/helpers.ts
+++ src/nest-modules/shared-modules/testing/helpers.ts
@@ -1,5 +1,7 @@
 import { INestApplication } from '@nestjs/common';
+import { getConnectionToken } from '@nestjs/sequelize';
 import { TestingModule, Test } from '@nestjs/testing';
+import { Sequelize } from 'sequelize-typescript';
 import { AppModule } from 'src/app.module';
 import { applyGlobalConfig } from 'src/nest-modules/global-config';
 
@@ -10,6 +12,9 @@ export const startApp = () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+    const sequelize = moduleFixture.get<Sequelize>(getConnectionToken());
+
+    await sequelize.sync({ force: true });
 
     _app = moduleFixture.createNestApplication();
     applyGlobalConfig(_app);
