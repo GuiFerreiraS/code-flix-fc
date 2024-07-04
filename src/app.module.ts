f741179bb7753c400fde4d7ed429eaf4a321f3b1
Guilherme Ferreira de Souza <guilherme.ferreira@venturus.org.br>
2024-07-04 22:44:59 +0000

    cast members nest module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CategoriesModule } from './nest-modules/categories-module/categories.module';
-import { CastMembersModule } from './nest-modules/cast-member-module/cast-members.module';
+import { CastMembersModule } from './nest-modules/cast-members-module/cast-members.module';
 import { ConfigModule } from './nest-modules/config-module/config.module';
 import { DatabaseModule } from './nest-modules/database-module/database.module';
 import { SharedModule } from './nest-modules/shared-modules/shared.module';
