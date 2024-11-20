fdedc0415685717da8d1cfbd9cb6c7e8102bd39a
Guilherme Ferreira de Souza <guilherme.ferreira@venturus.org.br>
2024-11-20 13:24:16 -0300

    Eventos de integração vs eventos de domínio

--- src/core/video/domain/domain-events/video-audio-media-replaced.event.ts
+++ src/core/video/domain/domain-events/video-audio-media-replaced.event.ts
@@ -1,4 +1,7 @@
-import { IDomainEvent } from '@core/shared/domain/events/domain-event.interface';
+import {
+  IDomainEvent,
+  IIntegrationEvent,
+} from '@core/shared/domain/events/domain-event.interface';
 import { VideoId } from '../video.aggregate';
 import { Trailer } from '../trailer.vo';
 import { VideoMedia } from '../video-media.vo';
@@ -23,5 +26,28 @@ export class VideoAudioMediaReplacedEvent implements IDomainEvent {
     this.media_type = props.media_type;
     this.occurred_on = new Date();
     this.event_version = 1;
+  }
+
+  getIntegrationEvent(): VideoAudioMediaUploadedIntegrationEvent {
+    return new VideoAudioMediaUploadedIntegrationEvent(this);
+  }
+}
+
+export class VideoAudioMediaUploadedIntegrationEvent
+  implements IIntegrationEvent
+{
+  event_name: string;
+  payload: any;
+  occurred_on: Date;
+  event_version: number;
+
+  constructor(event: VideoAudioMediaReplacedEvent) {
+    this.event_version = event.event_version;
+    this.occurred_on = event.occurred_on;
+    this.payload = {
+      video_id: event.aggregate_id.id,
+      media: event.media.toJSON(),
+    };
+    this.event_name = this.constructor.name;
   }
 }
