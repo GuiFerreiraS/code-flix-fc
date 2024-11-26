ca2c93a003716bf6c425b74d42277d6bb959d7ef
Guilherme Ferreira de Souza <guilherme.ferreira@venturus.org.br>
2024-11-25 21:23:39 -0300

    Autenticação JWT no Nest

--- src/nest-modules/auth-module/auth.guard.ts
+++ src/nest-modules/auth-module/auth.guard.ts
@@ -11,21 +11,25 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
-  
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request: Request = context.switchToHttp().getRequest();
     //Bearer XXXXX
     const token = request.headers.authorization?.split(' ')[1];
+
+    console.log({ token, service: this.jwtService });
     if (!token) {
       throw new UnauthorizedException();
     }
     try {
-      const payload = this.jwtService.verify(token, { secret: '123456' });
+      const payload = this.jwtService.verify(token, { algorithms: ['RS256'] });
+      console.log({ payload });
       request['user'] = payload;
       return true;
     } catch (e) {
+      console.log('Error' + e);
       throw new UnauthorizedException();
     }
   }
