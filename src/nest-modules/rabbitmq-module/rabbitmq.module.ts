b7978978ff39719079d3a3e5611cfaf43c36ca63
Guilherme Ferreira de Souza <guilherme.ferreira@venturus.org.br>
2024-12-20 08:31:30 -0300

    Fix rabbitmq

--- src/nest-modules/rabbitmq-module/rabbitmq.module.ts
+++ src/nest-modules/rabbitmq-module/rabbitmq.module.ts
@@ -60,7 +60,6 @@ export class RabbitmqModule {
                 name: 'dlx.queue',
                 exchange: 'dlx.exchange',
                 routingKey: '#', //aceita qualquer routing key
-                createQueueIfNotExists: false,
               },
             ],
           }),
